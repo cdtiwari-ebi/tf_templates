0bc9b438f57b5c1f14fef041677296b59800fbdb
C.D. Tiwari <cdtiwari@live.in>
2019-03-14 10:58:10 +0000

    First cut to create vpc on aws and gcp completed.

---
+++ connection.tf
@@ -0,0 +1,20 @@
+provider "google" {
+  credentials = "${file("../account.json")}"
+  project = "extreme-lore-114513"
+  region = "us-west1"
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+provider "azure" {
+
+}
+
+provider "azurerm" {
+  subscription_id = "0"
+  client_id = "1"
+  client_secret = "2"
+  tenant_id = "3"
+}
