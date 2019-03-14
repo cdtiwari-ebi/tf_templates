0bfc2bde1827d505c8638f2e84a33d0b37f773ac
C.D. Tiwari <cdtiwari@live.in>
2019-03-14 17:00:10 +0000

    TF network and subdomain

--- resource.tf
+++ resource.tf
@@ -1,6 +1,13 @@
 resource "google_compute_network" "our_development_network" {
   name = "devnetwork"
-  auto_create_subnetworks = true
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "dev-subnet" {
+  ip_cidr_range = "10.0.1.0/24"
+  name = "${google_compute_network.our_development_network.name}"
+  network = "${google_compute_network.our_development_network.self_link}"
+  region = "us-west1"
 }
 
 resource "aws_vpc" "env_example_two" {
@@ -9,5 +16,28 @@ resource "aws_vpc" "env_example_two" {
   enable_dns_support = true
   tags {
     name = "tf-aws-vpc-example-two"
+  }
+}
+
+resource "aws_subnet" "subnet1" {
+  cidr_block = "${cidrsubnet(aws_vpc.env_example_two.cidr_block, 3, 1)}"
+  vpc_id = "${aws_vpc.env_example_two.id}"
+  availability_zone = "us-west-2a"
+}
+
+resource "aws_subnet" "subnet2" {
+  cidr_block = "${cidrsubnet(aws_vpc.env_example_two.cidr_block, 2, 2)}"
+  vpc_id = "${aws_vpc.env_example_two.id}"
+  availability_zone = "us-west-2b"
+}
+
+resource "aws_security_group" "subnetsecurity" {
+  vpc_id = "${aws_vpc.env_example_two.id}"
+
+  ingress {
+    from_port = 80
+    protocol = "tcp"
+    to_port = 80
+    cidr_blocks = ["${aws_vpc.env_example_two.cidr_block}"]
   }
 }
---
+++ servers.tf
@@ -0,0 +1,54 @@
+resource "google_compute_instance" "firstserver" {
+  name = "thefirstserver"
+  "boot_disk" {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+
+  }
+  machine_type = "n1-standard-1" //n1-standard-1
+  zone = "us-west1-a"
+  "network_interface" {
+    subnetwork = "${google_compute_subnetwork.dev-subnet.name}"
+    access_config {}
+  }
+
+  metadata {
+    foo = "bar"
+  }
+  //Permission operation within Google cloud platform
+//  service_account {
+//    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+//  }
+}
+
+// Terraform plugin for creating random ids
+//resource "random_id" "instance_id" {
+// byte_length = 8
+//}
+
+// A single Google Cloud Engine instance
+//resource "google_compute_instance" "default" {
+// name         = "flask-vm-${random_id.instance_id.hex}"
+// machine_type = "f1-micro"
+// zone         = "us-west1-a"
+//
+// boot_disk {
+//   initialize_params {
+//     image = "debian-cloud/debian-9"
+//   }
+// }
+//
+//// Make sure flask is installed on all new instances for later steps
+// metadata_startup_script = "sudo apt-get update; sudo apt-get install -yq build-essential python-pip rsync; pip install flask"
+//
+// network_interface {
+////   network = "default"
+//   subnetwork = "${google_compute_subnetwork.dev-subnet.name}"
+//
+//   access_config {
+//     // Include this section to give the VM an external ip address
+//   }
+// }
+//}
+
