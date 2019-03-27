fe032ed33656f1c372ee24532b584c106db0439d
C.D. Tiwari <cdtiwari@live.in>
2019-03-27 16:00:52 +0000

    Clean way of doing for dev env.

--- aws/dev/main.tf
+++ aws/dev/main.tf
@@ -1,19 +1,19 @@
 provider "aws" {
-  region     = "us-west-2"
+  region = "${var.aws_region}"
 }
 
 module "dev_vpc" {
   source      = "../modules/vpc"
-  vpc_cidr    = "192.168.0.0/18"
-  tenancy     = "default"
-  vpc_id      = "${module.dev_vpc.vpc_id}"
-  subnet_cidr = "192.168.1.0/24"
+  vpc_id      = "${module.dev_vpc.vpc_id}" # This is output variable from module
+  vpc_cidr    = "${var.vpc_cidr}"
+  tenancy     = "${var.tenancy}"
+  subnet_cidr = "${var.subnet_cidr}"
 }
 
 module "dev_ec2" {
   source        = "../modules/ec2"
-  ec2_count     = "1"
-  instance_type = "t2.micro"
-  subnet_id     = "${module.dev_vpc.subnet_id}"
-  ami_id        = "ami-005bdb005fb00e791"
+  subnet_id     = "${module.dev_vpc.subnet_id}" # This is output variable from module
+  ec2_count     = "${var.ec2_count}"
+  instance_type = "${var.instance_type}"
+  ami_id        = "${var.ami_id}"
 }
---
+++ aws/dev/vars.tf
@@ -0,0 +1,32 @@
+# AWS common variables
+variable "aws_region" {
+  default = "us-west-2"
+  description = "aws region code, please refer https://docs.aws.amazon.com/general/latest/gr/rande.html"
+}
+
+variable "tenancy" {
+  default = "default"
+  description = "Name of the tenancy"
+}
+
+variable "ami_id" {
+  default = "ami-005bdb005fb00e791" # AMI Id's always vary from region to region, this is valid for us-west-2 only
+}
+
+# Networking variables
+variable "vpc_cidr" {
+  default = "192.168.0.0/18"
+}
+
+variable "subnet_cidr" {
+  default = "192.168.1.0/24"
+}
+
+# Compute variables
+variable "ec2_count" {
+  default = "1"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
